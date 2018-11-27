b16c30e5b21f6e59efe480c10cba1000a5c7738b
Justin <DoNotEmailMe@email.com>
2018-11-26 20:04:40 -0500

    init

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM clojure as builder
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+COPY project.clj /usr/src/app/
+RUN lein deps
+COPY . /usr/src/app
+RUN mv "$(lein uberjar | sed -n 's/^Created \(.*standalone\.jar\)/\1/p')" app-standalone.jar
+
+FROM jam/graal as native
+WORKDIR /tmp
+COPY --from=builder /usr/src/app/app-standalone.jar ./
+RUN graalvm-ce-${GRAALVM_V}/bin/native-image \
+  --no-server \
+  --static \
+  -H:+ReportUnsupportedElementsAtRuntime \
+  -jar /tmp/app-standalone.jar
+
+FROM scratch
+COPY --from=native /tmp/app-standalone .
+ENTRYPOINT ["/app-standalone"]
---
+++ src/jyvert/core.clj
@@ -0,0 +1,26 @@
+(ns jyvert.core
+  (:gen-class)
+  (:require [yaml.core :as yaml])
+  (:require [clojure.data.json :as json])
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as strs])
+  (:require [clojure.tools.logging :as log]))
+
+(defn convert
+  "Converts yaml to json and json to yaml"
+  [file]
+  (if (and (.exists (io/file file)) (not (.isDirectory (io/file file))))
+    (cond
+      (strs/ends-with? file ".json") (spit (strs/replace file #".json" ".yml") (yaml/generate-string (json/read-str (slurp file))))
+      (strs/ends-with? file ".yaml") (spit (strs/replace file #".yaml" ".json") (json/write-str (yaml/parse-string (slurp file))))
+      :else (log/info "Unknown extension for:" file))
+    (log/info "File not found or is a directory.")))
+
+(defn -main
+  "Converts file path cli arguments to either .json or .yaml files."
+  [& args]
+  (if-not (empty? args)
+    (do
+      (dorun (pmap convert args))
+      (shutdown-agents))
+    (log/info "Supply either .json or .yaml file paths.")))
