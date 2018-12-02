f7ac9dcc8ad57a335a30a37390b9c4f61b85480c
Justin <DoNotEmailMe@email.com>
2018-12-02 13:09:56 -0500

    deps-make

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0-RC2" :scope "provided"]
                  [io.forward/yaml "1.0.9" :exclusions [org.yaml/snakeyaml org.clojure/clojure]]
                  ; snakeyaml fixed graal issues in version > 1.22
                  [org.yaml/snakeyaml "1.23"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/tools.logging "0.4.1"]
+                 [org.clojure/data.json "0.2.6" :exclusions[org.clojure/clojure]]
+                 [org.clojure/tools.logging "0.4.1" :exclusions [org.clojure/clojure]]
                  ]
   :main ^:skip-aot jyvert.core
   ; reduces binary size
